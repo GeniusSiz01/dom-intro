b1541516c2f1a31f4f0e25fb9c8061b6e606a66f
GeniusSiz01 <sizwe.msimango01@gmail.com>
2021-04-29 17:26:28 +0200

    my initial commit

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,3 +1,49 @@
+const billItemType = document.querySelector(".billItemTypeRadio")
+
+const radioBillAddBtn = document.querySelector(".radioBillAddBtn")
+
+const callTotalTwo = document.querySelector(".callTotalTwo")
+
+const smsTotalTwo = document.querySelector(".smsTotalTwo")
+
+const totalCostElemen = document.querySelector(".totalTwo")
+
+
+
+
+var callsTotal = 0
+var smsTotal = 0
+function selectBilladdBtn() {
+
+    var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
+if (checkedRadioBtn){
+    var billItemType = checkedRadioBtn.value
+    // billItemType will be 'call' or 'sms'
+}
+
+    if ( billItemType === "call") {
+        callsTotal += 2.75
+    }
+    else if (billItemType === "sms") {
+        smsTotal += 0.75
+    }
+    callTotalTwo.innerHTML = callsTotal.toFixed(2);
+    smsTotalTwo.innerHTML = smsTotal.toFixed(2);
+    var totalBill = callsTotal + smsTotal;
+    totalCostElemen.innerHTML = totalBill.toFixed(2);
+    
+    //color the total based on the criteria
+    if (totalBill >= 50){
+        // adding the danger class will make the text red
+        totalCostElemen.classList.add("danger");
+    }
+    else if (totalBill >= 30){
+        totalCostElemen.classList.add("warning");
+    }
+}
+
+
+radioBillAddBtn.addEventListener("click", selectBilladdBtn)
 // get a reference to the sms or call radio buttons
 
 //get a reference to the add button
